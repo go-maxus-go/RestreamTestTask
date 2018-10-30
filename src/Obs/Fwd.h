75d7e2e6f1d86b20a89016ef76332807335c2047
Maxim Sheshukov <sheshukovmaxim@gmail.com>
2018-10-30 19:55:29 +0300

    Add Obs module

---
+++ src/Obs/Fwd.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <memory>
+
+#include "Model/Fwd.h"
+
+
+namespace Obs
+{
+
+template<class T> struct Observer;
+
+using SlideObs = Observer<Model::Slide>;
+using SlideshowObs = Observer<Model::Slideshow>;
+
+using SlideObsPtr = std::shared_ptr<SlideObs>;
+using SlideshowObsPtr = std::shared_ptr<SlideshowObs>;
+
+} // namespace Obs
